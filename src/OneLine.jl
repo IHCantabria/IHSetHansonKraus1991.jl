82b6edb10263b8a1c36e2346bf5a7be161672700
defreitasL <lucas12.fp@hotmail.com>
2024-01-24 09:27:05 +0100

    Building

--- src/OneLine.jl
+++ src/OneLine.jl
@@ -448,7 +448,7 @@ function cal_OneLine()
 
     popr = best_candidate(resr)
 
-    Ymdr, q_tot = OneLine(yi, dt, dx, Hs, Tp, θ, depth, doc, exp(poprΧ[1]), X0, Y0, phi, bctype)
+    Ymdr, q_tot = OneLine(yi, dt, dx, Hs, Tp, θ, depth, doc, exp(popr[1]), X0, Y0, phi, bctype)
 
     Ysl = Ymdr[idx_obs,:]
     aRP = sum((Ysl.-mean(Ysl)).*(Y_obs .- mean(Y_obs)))/(std(Ysl)*std(Y_obs)*length(Ysl))
